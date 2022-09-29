3034f9b30cf1f39cce90c98d9805d9c0feac2b59
sabrina <misyellsabrina@gmail.com>
2022-09-29 15:02:20 +0700

    Change Password

--- lib/view/pages/account/account_page.dart
+++ lib/view/pages/account/account_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:picker/components/chart.dart';
+import 'package:picker/view/pages/account/changePw.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key, required this.changeNavbar}) : super(key: key);
@@ -50,41 +51,56 @@ class AccountPageState extends State<AccountPage> {
                 color: Color(0xFF777986),
                 fontSize: 16,
                 fontWeight: FontWeight.w500)),
-        Container(
-          padding: const EdgeInsets.all(10),
-          margin: const EdgeInsets.fromLTRB(33, 40, 33, 20),
-          height: 70,
-          decoration: const BoxDecoration(
-            color: Color(0xFFF3F4F8),
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    margin: const EdgeInsets.only(right: 10),
-                    height: 50,
-                    width: 50,
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    child: const Icon(
-                      Icons.lock_outline_rounded,
-                      color: Color((0xFF2CBF6C)),
+        GestureDetector(
+          onTap: () async {
+            final result = await Navigator.push<int>(
+                context,
+                MaterialPageRoute(
+                    // navigasi ke detail page, tapi detail pagenya pakai template
+                    // scaffold kalo perlu bawa index bottomnavbar nya sekalian
+                    builder: (context) => ChangePw()));
+            // abis page tujuan di pop, bawa indexnya ke parent
+            // widget buat ngeganti body widget kalo user pencet
+            // bottomnavbarnya waktu di page detail
+            if (result is int) widget.changeNavbar(result);
+          },
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.fromLTRB(33, 40, 33, 20),
+            height: 70,
+            decoration: const BoxDecoration(
+              color: Color(0xFFF3F4F8),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.only(right: 10),
+                      height: 50,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      child: const Icon(
+                        Icons.lock_outline_rounded,
+                        color: Color((0xFF2CBF6C)),
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Ubah Password',
-                    style: TextStyle(color: Colors.black, fontSize: 20),
-                  )
-                ],
-              ),
-              Icon(Icons.arrow_forward_ios_rounded, color: Color((0xFF2CBF6C)))
-            ],
+                    Text(
+                      'Change Password',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    )
+                  ],
+                ),
+                Icon(Icons.arrow_forward_ios_rounded,
+                    color: Color((0xFF2CBF6C)))
+              ],
+            ),
           ),
         ),
         Container(
---
+++ lib/view/pages/account/changePw.dart
@@ -0,0 +1,298 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChangePw extends StatefulWidget {
+  const ChangePw({super.key});
+
+  @override
+  State<ChangePw> createState() => _ChangePwState();
+}
+
+class _ChangePwState extends State<ChangePw> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  FocusNode myFocusNode = new FocusNode();
+  bool _secureText = true;
+
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+
+  // _showMsg(msg) {
+  //   final snackBar = SnackBar(
+  //     content: Text(msg),
+  //   );
+  //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  // }
+
+  final _currentIndex = 4;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Logged in uus',
+          style: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.power_settings_new, color: Colors.black),
+            onPressed: () {
+              print("anu");
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(40, 0, 0, 30),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Change Password',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+              child: TextFormField(
+                focusNode: myFocusNode,
+                controller: passwordController,
+                obscureText: _secureText,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+                      borderRadius: BorderRadius.circular(10)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: "Current Password",
+                  labelStyle: TextStyle(color: Color(0xFF2CBF6C)),
+                  suffixIcon: IconButton(
+                    onPressed: showHide,
+                    icon: Icon(
+                      _secureText ? Icons.visibility_off : Icons.visibility,
+                      color: Color(0xFF2CBF6C),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+              child: TextFormField(
+                focusNode: myFocusNode,
+                controller: passwordController,
+                obscureText: _secureText,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+                      borderRadius: BorderRadius.circular(10)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: "New Password",
+                  labelStyle: TextStyle(color: Color(0xFF2CBF6C)),
+                  suffixIcon: IconButton(
+                    onPressed: showHide,
+                    icon: Icon(
+                      _secureText ? Icons.visibility_off : Icons.visibility,
+                      color: Color(0xFF2CBF6C),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+              child: TextFormField(
+                focusNode: myFocusNode,
+                controller: passwordController,
+                obscureText: _secureText,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+                      borderRadius: BorderRadius.circular(10)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: "Confirm New Password",
+                  labelStyle: TextStyle(color: Color(0xFF2CBF6C)),
+                  suffixIcon: IconButton(
+                    onPressed: showHide,
+                    icon: Icon(
+                      _secureText ? Icons.visibility_off : Icons.visibility,
+                      color: Color(0xFF2CBF6C),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+                height: 50,
+                margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+                child: ElevatedButton(
+                  child:
+                      Text(_isLoading ? 'Please wait ...' : 'Change Password'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF2CBF6C),
+                    textStyle: TextStyle(fontSize: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    //_login();
+                  },
+                )),
+          ],
+        ),
+        //     child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Container(
+        //       padding: const EdgeInsets.fromLTRB(40, 0, 0, 30),
+        //       alignment: Alignment.centerLeft,
+        //       child: const Text(
+        //         'Change Password',
+        //         style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Container(
+        //       margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+        //       child: TextFormField(
+        //         focusNode: myFocusNode,
+        //         controller: passwordController,
+        //         obscureText: _secureText,
+        //         decoration: InputDecoration(
+        //           enabledBorder: OutlineInputBorder(
+        //               borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+        //               borderRadius: BorderRadius.circular(10)),
+        //           focusedBorder: OutlineInputBorder(
+        //               borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+        //               borderRadius: BorderRadius.circular(10)),
+        //           border:
+        //               OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        //           labelText: "Current Password",
+        //           labelStyle: TextStyle(color: Color(0xFF2CBF6C)),
+        //           suffixIcon: IconButton(
+        //             onPressed: showHide,
+        //             icon: Icon(
+        //               _secureText ? Icons.visibility_off : Icons.visibility,
+        //               color: Color(0xFF2CBF6C),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //     Container(
+        //       margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+        //       child: TextFormField(
+        //         focusNode: myFocusNode,
+        //         controller: passwordController,
+        //         obscureText: _secureText,
+        //         decoration: InputDecoration(
+        //           enabledBorder: OutlineInputBorder(
+        //               borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+        //               borderRadius: BorderRadius.circular(10)),
+        //           focusedBorder: OutlineInputBorder(
+        //               borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+        //               borderRadius: BorderRadius.circular(10)),
+        //           border:
+        //               OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        //           labelText: "New Password",
+        //           labelStyle: TextStyle(color: Color(0xFF2CBF6C)),
+        //           suffixIcon: IconButton(
+        //             onPressed: showHide,
+        //             icon: Icon(
+        //               _secureText ? Icons.visibility_off : Icons.visibility,
+        //               color: Color(0xFF2CBF6C),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //     Container(
+        //       margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+        //       child: TextFormField(
+        //         focusNode: myFocusNode,
+        //         controller: passwordController,
+        //         obscureText: _secureText,
+        //         decoration: InputDecoration(
+        //           enabledBorder: OutlineInputBorder(
+        //               borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+        //               borderRadius: BorderRadius.circular(10)),
+        //           focusedBorder: OutlineInputBorder(
+        //               borderSide: BorderSide(color: Color(0xFF2CBF6C)),
+        //               borderRadius: BorderRadius.circular(10)),
+        //           border:
+        //               OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        //           labelText: "Confirm New Password",
+        //           labelStyle: TextStyle(color: Color(0xFF2CBF6C)),
+        //           suffixIcon: IconButton(
+        //             onPressed: showHide,
+        //             icon: Icon(
+        //               _secureText ? Icons.visibility_off : Icons.visibility,
+        //               color: Color(0xFF2CBF6C),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //     Container(
+        //         height: 50,
+        //         margin: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+        //         child: ElevatedButton(
+        //           child: Text(_isLoading ? 'Please wait ...' : 'Sign In'),
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Color(0xFF2CBF6C),
+        //             textStyle: TextStyle(fontSize: 20),
+        //             shape: RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.circular(10),
+        //             ),
+        //           ),
+        //           onPressed: () {
+        //             //_login();
+        //           },
+        //         )),
+        //   ],
+        // )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          onTap: (index) => {Navigator.pop(context, index)},
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.stacked_bar_chart), label: "Ongoing"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.layers_rounded), label: "Queue"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.stacked_bar_chart), label: "Finished"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), label: "My Account")
+          ]),
+    );
+  }
+}
